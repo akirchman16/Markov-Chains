c8b25dfb772d99b14876d620ecde342891ae5123
akirchman16 <65667835+akirchman16@users.noreply.github.com>
2021-08-26 21:36:27 -0500

    Too many events were occurring to be shown

--- OnOff_01_Markov_082521.m
+++ OnOff_01_Markov_082521.m
@@ -1,21 +1,25 @@
-clear all;
+clearvars;
 close all;
 
 % Represents a simple 2 step Markov Chain (states 0-off and 1-on).
 
 State = 0;  %initial state of machine
-Log(1) = State;   %log of history of states
 
-P_01 = 0.7; %probability of state 0 going to state 1
+P_01 = 0.9; %probability of state 0 going to state 1
 P_00 = 1-P_01;  %probability of state 0 staying in state 0
 
-P_10 = 0.5; %probability of state 1 going to state 0
+P_10 = 0.3; %probability of state 1 going to state 0
 P_11 = 1-P_10;  %probability of state 1 staying in state 1
 
-Events = 1000;  %number of events that occur
+Events = 100;  %number of events that occur
+
+Color = zeros(Events,3);    %memory allocation for color of squares
+Step = zeros(1,Events);
+Log = zeros(1,Events+1);
+Log(1) = State;   %log of history of states
 
 for i = 1:Events
-    Step = i;
+    Step(i) = i;
     j = rand;   %random number to decide what occurs in step
     
     if State == 0
@@ -28,7 +32,15 @@ for i = 1:Events
         end
     end
     Log(i+1) = State;
+    if State == 1   %if machine is turned on...
+        Color(i+1,:) = [1 0 0]; %...square is red
+    else
+        Color(i+1,:) = [0 0 1];
+    end
 end
 
 figure(1);
-B = insertshape(
+for k = 1:Events
+    rectangle('Position',[(k-1) 0 1 1],'FaceColor',[Color(k+1,1) Color(k+1,2) Color(k+1,3)],'EdgeColor',[1 1 1]);
+end
+title('Red = On : Blue = Off');
